12474c145aabc3ff72c46d60a5fba94ec65b6001
Ruben Barkow-Kuder <github@r.z11.de>
2025-05-26 18:48:33 +0200

    set up webpack and AlpineJS project structure
    - Configure webpack with dev server on port 9090
    - Create modular component structure for all chapters
    - Implement responsive CSS styling for child-friendly UI
    - Add basic tonecolors implementation
    - Update run.sh with proper deployment instructions

--- dev/TODO.md
+++ dev/TODO.md
@@ -5,3 +5,5 @@ TODO
 - use webpack and node to run the app
 - use alpinejs for the frontend
 - use git for version control
+
+1257.5
---
+++ src/components/chords.js
@@ -0,0 +1,11 @@
+/**
+ * Chords component
+ */
+export function chords() {
+  return {
+    init() {
+      console.log('Chords component initialized');
+    }
+    // Component methods will be implemented later
+  };
+}
