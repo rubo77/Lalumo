9d9018024ab669caaded99da23999c0bc65a6bef
Ruben Barkow-Kuder <github@r.z11.de>
2025-05-27 17:04:33 +0200

    android app soll im fullscreen mode starten ohne statusbar oben und unten

--- android/app/src/main/java/com/lalumo/app/MainActivity.java
+++ android/app/src/main/java/com/lalumo/app/MainActivity.java
@@ -1,5 +1,25 @@
 package com.lalumo.app;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Enable fullscreen mode - hide both status bar and navigation bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        // For newer Android versions (API level 19+), better immersive mode
+        // This hides both the navigation bar and status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+}
